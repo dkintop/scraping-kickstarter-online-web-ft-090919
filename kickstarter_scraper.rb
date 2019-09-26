dfa6e2e00790dc1d2a21f9a3ac2bcc95a04c7dcb
Doug Kintop <dkintop@hotmail.com>
2019-09-26 17:49:29 +0000

    Automatically backed up by Learn

--- kickstarter_scraper.rb
+++ kickstarter_scraper.rb
@@ -9,7 +9,9 @@ def create_project_hash
     
     title = project.css("h2.bbcard_name strong a").text
     projects[title.to_sym] = {
-      :image_link => 
+      :image_link => project.css("div.project-thumbnail a img").attribute("src").value,
+      :description =>
+      
     }
   end 
 projects
