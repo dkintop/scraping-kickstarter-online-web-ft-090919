52d827efa257afddabbc9f1c861cb5be5c64f687
Doug Kintop <dkintop@hotmail.com>
2019-09-26 17:34:09 +0000

    Automatically backed up by Learn

--- kickstarter_scraper.rb
+++ kickstarter_scraper.rb
@@ -10,9 +10,11 @@ end
 create_project_hash
 
 # projects: kickstarter.css("li.project.grid_4")
+
 #title: project.css("h2.bbcard_name strong a").text
 
 #image link: project.css("div.project-thumbnail a img").attribute("src").value
+
 #description : project.css("p.bbcard_blurb").text
 
-project.css("span.location-name").text
+# location : project.css("span.location-name").text
